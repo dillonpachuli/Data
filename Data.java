e7e0d6a53685609be0979e551d5ff190c01d8dbe
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2025-04-28 16:33:08 +0000

    4.28.25

--- Data.java
+++ Data.java
@@ -1,8 +1,33 @@
-public class Data{
-    public static final int MAX = 0;
-    private int[][] grid;
-    public void repopulate(){
+import java.lang.Math;
 
+public class Data{
+    public static final int MAX = (int)(Math.random() * Integer.MAX_VALUE);
+
+    private int[][] grid;
+
+    public Data(int rows, int columns){
+        grid = new int[rows][columns];
+    }
+
+    public void repopulate(){
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length; j++){
+                while (grid[i][j] % 10 != 0 || grid[i][j] % 100 == 0) grid[i][j] = (int)(Math.random() * MAX + 1);
+            }
+        }
+    }
+
+    public int countIncreasingCols(){
+
+    }
+
+    public String toString(){
+        String s = "";
+        for (int[] row: grid){
+            for (int col: row) s += col + " ";
+            s += "\n";
+        }
+        return s;
     }
     
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String args[]){
+        Data d = new Data(4, 4);
+        d.repopulate();
+        System.out.println(d);
         
     }
 }
